3d4ebcf62cbecc5a42f5565e08fb6387cbdcf556
THEEALPHA254 <gracemuriithi2004@gmail.com>
2023-05-08 11:22:03 +0300

    first commit

---
+++ calc.py
@@ -0,0 +1,17 @@
+a=int(input("enter value a: "))
+b=input("enter an operator:")
+c=int(input("enter value c:"))
+if b== "+":
+   d=a+c
+elif b== "-":
+    d=a-c
+elif b== "*":
+    d=a*c
+else:
+    d=a/c
+print("The answer is", d)
+
+
+
+
+
